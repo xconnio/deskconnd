343aa7f99a918760e3c3166d334a6be4672d67a8
Omer Akram <om26er@gmail.com>
2019-08-05 02:11:25 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-VERSION = '0.9.0'
+VERSION = '0.1.0'
 
 setup(
-    name='deskconn',
+    name='deskconnd',
     version=VERSION,
-    packages=['deskconn', 'deskconn.components'],
-    url='https://github.com/deskconn/deskconn-server',
+    packages=['deskconnd', 'deskconnd.components'],
+    url='https://github.com/deskconn/deskconnd',
     license='GNU GPL Version 3',
     author='Omer Akram',
     author_email='om26er@gmail.com',
     description='Expose your desktop functionality over the network.',
-    download_url='https://github.com/deskconn/deskconn-server/tarball/{}'.format(VERSION),
+    download_url='https://github.com/deskconn/deskconnd/tarball/{}'.format(VERSION),
     keywords=['linux', 'ubuntu'],
 )
