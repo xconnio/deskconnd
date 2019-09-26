386338941dc1a6d59f02314c3fef01505a74f7ae
Omer Akram <om26er@gmail.com>
2019-09-26 23:29:52 +0500

    adapt to latest crossbar changes

--- server.sh
+++ server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir -p "$SNAP_COMMON"/crossbar-runtime-dir
-mkdir -p "$SNAP_COMMON"/crossbar-runtime-dir-2
 mkdir -p "$SNAP_COMMON"/deskconnd-sock-dir
 export DESKCONND_SOCK_DIR="$SNAP_COMMON"/deskconnd-sock-dir
 crossbar start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar/config.yaml
