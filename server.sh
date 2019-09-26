4cd665389cb99cc2576ce20af094c6a247d8a7bc
Omer Akram <om26er@gmail.com>
2019-09-26 21:29:37 +0500

    lets try to create the directories manually

--- server.sh
+++ server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p "$SNAP_COMMON"/crossbar-runtime-dir
+mkdir -p "$SNAP_COMMON"/crossbar-runtime-dir-2
 mkdir -p "$SNAP_COMMON"/deskconnd-sock-dir
 export DESKCONND_SOCK_DIR="$SNAP_COMMON"/deskconnd-sock-dir
 crossbar start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar/config.yaml
