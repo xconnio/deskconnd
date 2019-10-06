29dd0f2dd549fe06f360457c5e5f1f2229c75106
Omer Akram <om26er@gmail.com>
2019-10-06 20:31:32 +0500

    wait for crossbar interface to connect

--- server.sh
+++ server.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-mkdir -p "$SNAP_COMMON"/crossbar-runtime-dir
+SHOULD_PRINT=false
+
+while true
+do
+  if [ -f "$SNAP_COMMON"/crossbar-runtime-dir/bin/crossbar ]; then
+    echo "Crossbar found, now starting..."
+    break
+  else
+    if [ $SHOULD_PRINT = false ]; then
+      echo "Waiting for crossbar interface to connect"
+      SHOULD_PRINT=true
+    fi
+    sleep 1
+  fi
+done
+
 mkdir -p "$SNAP_COMMON"/deskconnd-sock-dir
 export DESKCONND_SOCK_DIR="$SNAP_COMMON"/deskconnd-sock-dir
 crossbar start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar/config.yaml
