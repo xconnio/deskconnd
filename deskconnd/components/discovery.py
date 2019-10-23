d9c6688642cd8dcae971d16ab884f1fd79099ed7
Omer Akram <om26er@gmail.com>
2019-10-23 14:28:47 +0300

    service discovery on all interfaces

--- deskconnd/components/discovery.py
+++ deskconnd/components/discovery.py
@@ -1,109 +1,67 @@
-import fcntl
 import os
 import socket
-import struct
-import time
 
 from autobahn.twisted import wamp
 from twisted.internet.task import LoopingCall
-from zeroconf import ServiceInfo, Zeroconf
+from twisted.internet import reactor
+import zeroconf
 
-SERVICE_TYPE = '_deskconn._tcp'
-FALLBACK_PORT = 5020
-
-
-def get_ip_address(iface):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    packed_ip = fcntl.ioctl(sock.fileno(), 0x8915, struct.pack('256s', iface[:15]))[20:24]
-    return socket.inet_ntoa(packed_ip)
-
-
-def get_default_gateway_linux():
-    with open("/proc/net/route") as fh:
-        for line in fh:
-            fields = line.strip().split()
-            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
-                continue
-            return fields[0]
-    return None
-
-
-def get_local_ip():
-    gateway = get_default_gateway_linux()
-    if gateway:
-        return get_ip_address(bytes(gateway, 'utf-8'))
-    return gateway
-
-
-def get_system_uid():
-    with open("/etc/machine-id") as file:
-        return file.read().strip()
-
-
-def initialize_service(ip):
-    return ServiceInfo(
-        type_="{}.local.".format(SERVICE_TYPE),
-        name="{}.{}.local.".format(socket.gethostname(), SERVICE_TYPE),
-        address=socket.inet_aton(ip),
-        port=os.environ.get("$DESKCONN_PORT", FALLBACK_PORT),
-        properties={"realm": "deskconn", "uid": get_system_uid()}
-    )
+SERVICE_IDENTIFIER = 'deskconn'
 
 
 class ServiceDiscoverySession(wamp.ApplicationSession):
     def __init__(self, config=None):
         super().__init__(config)
-        self.running = False
-        self.local_ip = None
-        self.zeroconf = Zeroconf()
-        self.repeated_call = None
-        self.unregister_on_connect = False
+        self.services = {}
+        self.looping_call = None
 
-    def check_has_ip(self):
-        current_ip = get_local_ip()
-        if self.running and not current_ip:
-            self.unregister_on_connect = True
-            self.local_ip = None
-            return
+    @classmethod
+    def get_system_uid(cls):
+        with open("/etc/machine-id") as file:
+            return file.read().strip()
 
-        if self.unregister_on_connect:
-            self.stop_publishing(True)
-            self.unregister_on_connect = False
+    def initialize_service_info(self, address):
+        return zeroconf.ServiceInfo(
+            type_=f"_{SERVICE_IDENTIFIER}._tcp.local.",
+            name=f"{socket.gethostname()}._{SERVICE_IDENTIFIER}._tcp.local.",
+            addresses=[socket.inet_aton(address)],
+            port=int(os.environ.get("DESKCONN_PORT")),
+            properties={"realm": "deskconn", "uid": self.get_system_uid()}
+        )
 
-        if self.local_ip and not current_ip:
-            self.stop_publishing()
-            self.local_ip = current_ip
-        elif not self.local_ip and current_ip:
-            self.local_ip = current_ip
-            self.start_publishing()
-        elif self.local_ip != current_ip:
-            self.local_ip = current_ip
-            self.start_publishing()
+    def initialize_service(self, address):
+        zconf = zeroconf.Zeroconf([address])
+        zconf.register_service(self.initialize_service_info(address), 20, True)
+        self.services.update({address: zconf})
+
+    def initialize_services(self):
+        for address in zeroconf.get_all_addresses():
+            self.initialize_service(address)
+
+    def close_services(self):
+        for service in self.services.values():
+            reactor.callInThread(service.close)
+        self.services.clear()
 
     async def onJoin(self, details):
         self.log.info('session joined: {}'.format(details))
-        self.repeated_call = LoopingCall(self.check_has_ip)
-        self.repeated_call.start(5)
-        self.check_has_ip()
+        reactor.callInThread(self.initialize_services)
+        self.looping_call = LoopingCall(self.sync_services)
+        self.looping_call.start(5, False)
 
     def onLeave(self, details):
         self.log.info('session left: {}'.format(details))
-        self.repeated_call.stop()
-        self.stop_publishing()
-        self.zeroconf.close()
-        self.disconnect()
+        self.close_services()
 
-    def start_publishing(self):
-        if self.running:
-            self.stop_publishing()
-        self.log.info("Registering service: {}".format(SERVICE_TYPE))
-        self.zeroconf.register_service(initialize_service(self.local_ip))
-        self.log.info("Registered service: {}".format(SERVICE_TYPE))
-        self.running = True
+    def sync_services(self):
+        addresses = zeroconf.get_all_addresses()
+        to_unregister = [address for address in self.services.keys() if address not in addresses]
+        to_register = [address for address in addresses if address not in self.services.keys()]
 
-    def stop_publishing(self, wait=False):
-        self.zeroconf.close()
-        if wait:
-            time.sleep(1)
-        self.running = False
-        self.log.info("Unregistered service: {}".format(SERVICE_TYPE))
+        def _sync():
+            for address in to_unregister:
+                self.services.pop(address).close()
+            for address in to_register:
+                self.initialize_service(address)
+
+        reactor.callInThread(_sync)
