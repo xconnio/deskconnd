bbc83a5db13df2d2b9606c8919831c2da04a8b9f
Omer Akram <om26er@gmail.com>
2019-10-30 23:08:20 +0500

    install crossbar inside our snap

--- scripts/daemon.sh
+++ scripts/daemon.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-SHOULD_PRINT=true
-
-while true
-do
-  if [ -f "$SNAP_COMMON"/crossbar-runtime-dir/crossbar/bin/crossbar ]; then
-    echo "Crossbar found, now starting..."
-    break
-  else
-    if [ $SHOULD_PRINT = true ]; then
-      echo "Waiting for crossbar runtime directory interface to connect"
-      SHOULD_PRINT=false
-    fi
-    sleep 1
-  fi
-done
-
 export DESKCONN_PORT=5020
-python3.8 -u "$(which crossbar)" start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar.yaml
+python3.8 -u "$SNAP"/packages/bin/crossbar start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar.yaml
--- setup.py
+++ setup.py
@@ -37,5 +37,5 @@ setup(
     description='Expose your desktop functionality over the network.',
     download_url='https://github.com/deskconn/deskconnd/tarball/{}'.format(VERSION),
     keywords=['IPC', 'python'],
-    install_requires=['appdirs', 'sqlalchemy', 'zeroconf']
+    install_requires=['crossbar', 'appdirs', 'sqlalchemy', 'zeroconf']
 )
