92d8c9edfeba91dcdef33920e164a77a28962f7a
Omer Akram <om26er@gmail.com>
2019-11-17 02:42:02 +0500

    remove py38 plugin

--- scripts/daemon.sh
+++ scripts/daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export DESKCONN_PORT=5020
-python3.8 -u "$SNAP"/packages/bin/crossbar start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar.yaml
+python3.8 -u "$SNAP"/bin/crossbar start --cbdir "$SNAP_USER_DATA" --config "$SNAP"/.crossbar.yaml
 rm -f $SNAP_COMMON/state/ready
