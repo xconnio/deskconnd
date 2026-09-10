c127b26c2d4beb0a4ef570b7519fa2f2b8255b2e
Muzzammil Shahid <muzzammil@xconn.io>
2026-09-10 14:14:59 +0500

    fix: inject cfgDirectory into NewDeskconn so tests don't hit the real DB

--- ai_test.go
+++ ai_test.go
@@ -24,7 +24,7 @@ func claudeProjectDir(homeDir, path string) string {
 func setupDeskconnWithInstance(t *testing.T) *xconn.Session {
 	t.Helper()
 	callee, caller := setupRouterAndConnectSessions(t)
-	d := deskconn.NewDeskconn(nil, nil, nil, false)
+	d := deskconn.NewDeskconn(nil, nil, nil, false, t.TempDir())
 	require.NoError(t, d.Register(callee))
 	return caller
 }
--- shell_test.go
+++ shell_test.go
@@ -16,7 +16,7 @@ import (
 func setupDeskconn(t *testing.T) (*xconn.Session, *xconn.Session) {
 	t.Helper()
 	callee, caller := setupRouterAndConnectSessions(t)
-	d := deskconn.NewDeskconn(nil, nil, nil, false)
+	d := deskconn.NewDeskconn(nil, nil, nil, false, t.TempDir())
 	require.NoError(t, d.Register(callee))
 	return callee, caller
 }
